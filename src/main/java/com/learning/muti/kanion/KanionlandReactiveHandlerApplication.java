e21de8b67131588a179536f5236495608ec062df
Esteban Hernandez <mutisantos@hotmail.com>
2025-01-09 16:16:37 -0500

    Spring Boot base commit

---
+++ src/main/java/com/learning/muti/kanion/KanionlandReactiveHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.learning.muti.kanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanionlandReactiveHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanionlandReactiveHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/muti/kanion/KanionlandReactiveHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.muti.kanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanionlandReactiveHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
